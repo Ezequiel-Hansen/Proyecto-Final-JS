4f299fc030583784ff6fd3387ca7072bd68cb8fb
Ezequiel Hansen <82328876+Ezequiel-Hansen@users.noreply.github.com>
2026-02-07 11:56:14 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ class BD{
         this.productos=[]
     }
     async traeRegistro(){
-        const response= await fetch("../json/productos.json");
+        const response= await fetch("./json/productos.json");
         this.productos=await response.json();
         return this.productos;
         
@@ -241,5 +241,6 @@ function comprarTodoBoton(){
           })
     });
 }
+
 
 
